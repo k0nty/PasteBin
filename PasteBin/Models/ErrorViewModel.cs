dfa5e9324978d58f711c3da00a11d0f5c225eec6
Denys Prokopiuk <prokopiuckdenys250904@gmail.com>
2024-01-23 01:16:02 +0200

    Add project files.

---
+++ PasteBin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PasteBin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
